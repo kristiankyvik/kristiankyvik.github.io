bcfa735c9fbd163a84dcaeff69d78e67179ba56d
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2015-12-04 10:30:55 +0100

    now tracking events

---
+++ js/global.js
@@ -0,0 +1,11 @@
+mixpanel.track_links("#ntnu", "Clicked NTNU Link");
+mixpanel.track_links("#toad", "Clicked TOAD Link");
+mixpanel.track_links("#ih", "Clicked IronHack Link");
+
+mixpanel.track_links("#cv", "Clicked CV Link");
+mixpanel.track_links("#ld", "Clicked LinkedIn Link");
+mixpanel.track_links("#gh", "Clicked GutHub Link");
+mixpanel.track_links("#ml", "Clicked Contact Mail Link");
+
+mixpanel.track_links("#wt", "Clicked Worldtrip Link");
+mixpanel.track_links("#ba", "Clicked Biblioteca Almirall Link");
